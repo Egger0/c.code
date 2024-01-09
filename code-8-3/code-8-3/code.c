0888b488071aaff642355f9bbf6bbb37dbc024d5
egger <2383749754@qq.com>
2024-01-09 14:32:13 +0800

    test.2

---
+++ code-8-3/code-8-3/code.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+int main() {	
+		 char Ãû×Ö = 'w';
+		 short age = 19;
+		 int weight = 65;
+		 long height = 178;
+		 long id = 360429;
+		 long ca = 20040601;
+		 long rd = 1512;
+		 float birthdate = 6.1;
+		 double todaydate = 8.3;
+		 printf("%c\n%d\n%d\n%d\n%d%d%d\n%f\n%lf\n", Ãû×Ö, age, weight, height, id,ca,rd, birthdate, todaydate);
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		return 0;
+}                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                             
