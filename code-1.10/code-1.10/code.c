8008adc66b4a61ea2751660baf25428891b35642
egger <2383749754@qq.com>
2024-01-10 16:14:10 +0800

    20240110代码练习

---
+++ code-1.10/code-1.10/code.c
@@ -0,0 +1,23 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+int main()
+{
+	 int  a = 0;
+		printf("请输入密码\n");
+		scanf("%d", &a);
+		if (a ==2383749754)
+			printf("密码正确");
+		else
+			printf("密码不正确");
+		
+
+
+		
+
+
+	
+
+
+	
+     return 0;
+}
